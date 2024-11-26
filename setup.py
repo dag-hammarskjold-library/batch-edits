9c0fa9be2c2857ca6905e32abbe670af71d50758
jbukhari <joe.bukhari@gmail.com>
2024-11-26 14:52:42 -0500

    Bump version
    Closes #18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 NAME = 'batch-edits'
 DESCRIPTION = ''
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 with open("README.md") as f:
     long_description = f.read()
