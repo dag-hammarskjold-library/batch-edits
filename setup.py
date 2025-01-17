8bcc74f8eff7fc88345bbeb5eec474db589d9b4a
jbukhari <joe.bukhari@gmail.com>
2025-01-16 19:05:00 -0500

    Bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 NAME = 'batch-edits'
 DESCRIPTION = ''
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 with open("README.md") as f:
     long_description = f.read()
