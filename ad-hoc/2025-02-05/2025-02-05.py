b790cad599fcd13f9a8b9f26f7ea57ebed373d0f
jbukhari <jbukhari@gmail.com>
2025-02-05 13:59:52 -0500

    finalize

--- ad-hoc/2025-02-05/2025-02-05.py
+++ ad-hoc/2025-02-05/2025-02-05.py
@@ -1,15 +1,19 @@
 # https://ask.un.org/admin/ticket?qid=14959124
 # delete the 856 values specified in data.txt
 
-import sys, os, re
+import sys
 from dlx import DB
 from dlx.marc import Bib
 
 DB.connect(sys.argv[1], database=sys.argv[2])
 
 for line in open(sys.argv[3]).readlines()[1:]:
+    line = line.strip()
     rid, link = line.split('\t')
-    #print([rid, link])
     bib = Bib.from_id(int(rid))
-    to_delete = next(filter(lambda x: link == x.get_value('u'), bib.get_fields('856')), None)
-    print(to_delete)
+
+    # get the field with the deal link
+    if field := next(filter(lambda x: link == x.get_value('u'), bib.get_fields('856')), None):
+        print(f'Deleting field and saving bib# {rid}')
+        bib.delete_field(field)
+        bib.commit(user='admin')
