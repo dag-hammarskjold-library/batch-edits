8f67dcb0b7d4d6dfb50eaf90102c39313c23c2b9
jbukhari <joe.bukhari@gmail.com>
2025-04-15 11:20:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 NAME = 'batch-edits'
 DESCRIPTION = ''
-VERSION = '1.0.5'
+VERSION = '1.0.6'
 
 with open("README.md") as f:
     long_description = f.read()
