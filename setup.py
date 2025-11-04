acb325bdfb6e70a12541364a68a0a81ef34ba38f
jbukhari <joe.bukhari@gmail.com>
2025-11-04 13:10:35 -0500

    Change version to 1.0.8.dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 NAME = 'batch-edits'
 DESCRIPTION = ''
-VERSION = '1.0.8'
+VERSION = '1.0.8.dev'
 
 with open("README.md") as f:
     long_description = f.read()
